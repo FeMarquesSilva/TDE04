11afab216e1923287912809b4c983640a8aa966d
FelipeMarques <felipemarques330@gmail.com>
2024-08-29 21:00:17 -0300

    Adicionando começo do TDE

---
+++ my-app/app/_layout.tsx
@@ -0,0 +1,23 @@
+import { ScrollView, StyleSheet } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { Stack } from "expo-router";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#e3bde3",
+    padding: 50,
+  },
+});
+
+export default function RootLayout() {
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
+
+  return (
+    <Stack>
+      <Stack.Screen name="pages" />
+    </Stack>
+  );
+}
